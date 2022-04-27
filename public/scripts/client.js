55adc0cbb92a11f5a50dbd3053a9a655a59953da
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-04-27 18:10:39 -0500

    add renderTweet function to append tweets dynamically

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   /* Mock Tweet Data */
-  const data = [
+  const tweetsDB = [
     {
       "user": {
         "name": "Newton",
@@ -10,7 +10,7 @@ $(document).ready(function() {
         "handle": "@SirIsaac"
       },
       "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
+        "text": "If I have seen further, it is by standing on the shoulders of giants."
       },
       "created_at": 1461116232227
     },
@@ -25,36 +25,47 @@ $(document).ready(function() {
       "created_at": 1461113959088
     }
   ]
-  
-  /* function that creates new tweet html markup */
-  const createTweetElement = function(tweet) {
-    
+
+  /* function that renders tweets from DB, and appends to HTML index  */
+  const renderTweets = function(tweetsDB) {
+    let $styledTweet = '';
+
+    for (const tweet of tweetsDB) {
+      $styledTweet = createTweetElement(tweet);
+      $("#tweets-container").append($styledTweet)
+    }
+  }
+
+  /* function that creates new tweet elements in HTML markup */
+  const createTweetElement = function({user, content, created_at}) {
     /* Selectors */
-    const $tweetsContainer = $("#tweets-container");
     const $article = $("<article>");
     const $header = $("<header>");
     const $userInfo = $("<div>").addClass("user-info");
     const $userIcon = $("<i>").addClass("fa-solid fa-meteor");
-    const $username = $("<h3>").addClass("name").text("Newton");
-    const $userHandle = $("<h3>").addClass("user").text("@SirIsaac");
+    const $username = $("<h3>").addClass("name").text(`${user.name}`);
+    const $userHandle = $("<h3>").addClass("user").text(`${user.handle}`);
     const $userTweetDiv = $("<div>").addClass("user-tweet");
-    const $userTweet = $("<p>").text("If I have seen further, it is by standing on the shoulders of giants.");
+    const $userTweet = $("<p>").text(`${content.text}`);
     const $tweetFooter = $("<footer>");
-    const $timeOfTweet = $("<span>").text("10 days ago");
+    const $timeOfTweet = $("<span>").text(`${created_at}`);
     const $socialIconDiv = $("<div>");
     const $bookmarkIcon = $("<i>").addClass("fas fa-bookmark");
     const $retweetIcon = $("<i>").addClass("fas fa-retweet");
     const $heartIcon = $("<i>").addClass("fas fa-heart");
 
     /* Apending HTML elements to form new tweet structure. */
-    $tweetsContainer.append($article);
     $article.append($header, $userTweetDiv, $tweetFooter);
     $header.append($userInfo, $userHandle);
     $userInfo.append($userIcon, $username);
     $userTweetDiv.append($userTweet);
     $tweetFooter.append($timeOfTweet, $socialIconDiv);
     $socialIconDiv.append($bookmarkIcon, $retweetIcon, $heartIcon);
+    
+    return $article;
   };
-  createTweetElement(data);
+
+  renderTweets(tweetsDB);
+ 
 });
 

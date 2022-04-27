ee0f2d81b29a7390f3c1abfde9cf4c66d9c32bb1
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-04-27 16:47:00 -0500

    change HTML injection from template literal to creating text node

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,57 +1,60 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-/* Selectors */
-
-/* Mock Tweet Data */
-const tweetObject =  {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further, it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-};
-
-
-/* function that creates new tweet html markup */
-const createTweetElement = function({user, content, created_at}) {
-  
-  const $tweet = `
-  <article>
-    <header>
-      <div class="user-info">
-        <i class="fa-solid fa-meteor"></i>
-        <h3 class="name">${user.name}</h3>
-      </div>
-      <h3 class="user">${user.handle}</h3>
-    </header>
-    <div class="users-tweet">
-      <p>${content.text}</p>
-    </div>
-    <footer>
-      <span>${created_at}</span>
-      <span>
-        <i class="fas fa-bookmark"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart"></i>
-      </span>
-    </footer>
-  </article>
-  `;
-
-  $(".tweets-container").append($tweet);
-};
-
-
-
 $(document).ready(function() {
-  createTweetElement(tweetObject);
+
+  /* Mock Tweet Data */
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+  
+  /* function that creates new tweet html markup */
+  const createTweetElement = function(tweet) {
+    
+    /* Selectors */
+    const $tweetsContainer = $("#tweets-container");
+    const $article = $("<article>");
+    const $header = $("<header>");
+    const $userInfo = $("<div>").addClass("user-info");
+    const $userIcon = $("<i>").addClass("fa-solid fa-meteor");
+    const $username = $("<h3>").addClass("name").text("Newton");
+    const $userHandle = $("<h3>").addClass("user").text("@SirIsaac");
+    const $userTweetDiv = $("<div>").addClass("user-tweet");
+    const $userTweet = $("<p>").text("If I have seen further, it is by standing on the shoulders of giants.");
+    const $tweetFooter = $("<footer>");
+    const $timeOfTweet = $("<span>").text("10 days ago");
+    const $socialIconDiv = $("<div>");
+    const $bookmarkIcon = $("<i>").addClass("fas fa-bookmark");
+    const $retweetIcon = $("<i>").addClass("fas fa-retweet");
+    const $heartIcon = $("<i>").addClass("fas fa-heart");
+
+    /* Apending HTML elements to form new tweet structure. */
+    $tweetsContainer.append($article);
+    $article.append($header, $userTweetDiv, $tweetFooter);
+    $header.append($userInfo, $userHandle);
+    $userInfo.append($userIcon, $username);
+    $userTweetDiv.append($userTweet);
+    $tweetFooter.append($timeOfTweet, $socialIconDiv);
+    $socialIconDiv.append($bookmarkIcon, $retweetIcon, $heartIcon);
+  };
+  createTweetElement(data);
 });
 

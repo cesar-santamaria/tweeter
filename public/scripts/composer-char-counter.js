bd46e46f36a3c7ddf0679794ee416b5aa2331da9
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-04-26 20:15:45 -0500

    add character count functionality and CSS styling to display when user has exceeded max characters available

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
   // event listener on textarea element in main section
-  $("#tweet-text").on( "keypress",function() {
-    const textarea = $(this);
-    const characterCount = textarea.val().length;
-    let count = 140;
-    if (characterCount) {
-      count = count - characterCount;
+  $("#tweet-text").on( "input",function() {
+    const $textarea = $(this);
+    const characterCount = $textarea.val().length; // getting total amount of characters inputted
+    const $tweetForm = $(this).parent() // traversing up the DOM to parent element (form)
+    const outputCounter = $tweetForm.find(".counter") //traversing down the DOM to child element (output)
+
+    outputCounter.html(140 - characterCount); // change html to render updated character count
+
+    // change text to color red if max character count is exceeded. 
+    if (characterCount > 140) {
+      outputCounter.addClass('over-count');
+      outputCounter.html(140 - characterCount + " (Max character exceeded)")
+    } else {
+      outputCounter.removeClass('over-count');
     }
-    console.log(count)
   })
 
 });

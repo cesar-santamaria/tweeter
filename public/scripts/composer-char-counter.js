ca9f9706ef9502fcdea797b160b8a9a5a6701f80
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-04-26 20:22:06 -0500

    fix linting errors

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,19 @@
 $(document).ready(function() {
-  // event listener on textarea element in main section
-  $("#tweet-text").on( "input",function() {
+  $("#tweet-text").on("input", function() {
     const $textarea = $(this);
-    const characterCount = $textarea.val().length; // getting total amount of characters inputted
-    const $tweetForm = $(this).parent() // traversing up the DOM to parent element (form)
-    const outputCounter = $tweetForm.find(".counter") //traversing down the DOM to child element (output)
+    const characterCount = $textarea.val().length; // getting total amount of characters inputted.
+    const $tweetForm = $(this).parent(); // traversing up the DOM to parent element (form).
+    const outputCounter = $tweetForm.find(".counter"); //traversing down the DOM to child element (output).
 
-    outputCounter.html(140 - characterCount); // change html to render updated character count
+    outputCounter.html(140 - characterCount); // change html to render updated character count.
 
-    // change text to color red if max character count is exceeded. 
+    // add class to change text color if max character count is exceeded.
     if (characterCount > 140) {
       outputCounter.addClass('over-count');
-      outputCounter.html(140 - characterCount + " (Max character exceeded)")
+      outputCounter.html(140 - characterCount + " (Max character exceeded)");
     } else {
       outputCounter.removeClass('over-count');
     }
-  })
+  });
 
 });

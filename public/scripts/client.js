48250f697ebc96d87b6871ac70f49c7030441a7c
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-04-27 19:43:07 -0500

    add ajax GET request to /tweets path and render them on HTML index

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -13,30 +13,7 @@ $(document).ready(function() {
   });
 
   /* Mock Tweet Data */
-  const tweetsDB = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further, it is by standing on the shoulders of giants."
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+  const tweetsDB = [];
 
 
   /* function that renders tweets from DB, and appends to HTML index  */
@@ -78,10 +55,15 @@ $(document).ready(function() {
     
     return $article;
   };
-
   renderTweets(tweetsDB);
 
-
- 
+  const fetchTweets = function() {
+    $.ajax('/tweets', { method: 'GET' })
+    .then(function (data) {
+      console.log('Success: ', data);
+      renderTweets(data)
+    })
+  };
+  fetchTweets()
 });
 
